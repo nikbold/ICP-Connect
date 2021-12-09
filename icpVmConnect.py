178d9afeae2c6503912514cb67ad720d27d605fb
nikbold <nik.bold@zynstra.com>
2021-12-09 09:45:58 +0000

    Added basic framework of App
    Added basic framework of App

--- icpVmConnect.py
+++ icpVmConnect.py
@@ -1,1 +1,14 @@
-# Add 
+#!/usr/bin/python
+
+# Import Library's
+import sys
+import mariadb
+
+# Welcome message
+# Ask for ICP name input
+# Connect to db and get ICP IP
+# Ask for VM name
+# Connect to db and get VM last octet
+# Combine ip's for VM and print
+# ssh to the VM 
+# return to start
